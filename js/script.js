1499dc714b7ba3252bc5a95bc3d8e1f9dc4b6e31
BrSamedi <z13s12@yandex.ru>
2019-08-01 18:49:32 +0300

    add slider

--- js/script.js
+++ js/script.js
@@ -161,5 +161,58 @@ window.addEventListener('DOMContentLoaded', function() { //загрузилос�
             input[i].value = '';
         }
     });
+
+    //Slider
+
+    let slideIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+    
+    showSlides(slideIndex);
+
+    function showSlides(n) {
+        if (n > slides.length) {
+            slideIndex = 1;
+        }
+        if (n < 1) {
+            slideIndex = slides.length;
+        }
+        // for (let i=0; i < slides.length; i++) { //старый метод
+            //     slides[i].style.display = 'none';
+            // }
+        slides.forEach((item) => item.style.display = 'none');//новый
+        dots.forEach((item) => item.classList.remove('dot-active'));
+
+        slides[slideIndex-1].style.display = 'block';
+        dots[slideIndex-1].classList.add('dot-active');
+        
+    }
+
+    function plusSlides(n) {
+        showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+        showSlides(slideIndex = n)
+    }
+
+    prev.addEventListener('click', function() {
+        plusSlides(-1);
+    });
+
+    next.addEventListener('click', function() {
+        plusSlides(1);
+    });
+
+    dotsWrap.addEventListener('click', function(event) {
+        for (let i =0; i < dots.length + 1; i++) {
+            if (event.target.classList.contains('dot') && event.target == dots[i-1]) {
+                currentSlide(i);
+            }
+        }
+    });
 }); 
 
